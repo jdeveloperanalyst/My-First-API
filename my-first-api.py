98cc82c958f911c35a9fa7dac832743285597585
jdeveloperanalyst <jonatas1200@icloud.com>
2023-04-14 20:10:08 -0300

    conexão com o banco ok

--- my-first-api.py
+++ my-first-api.py
@@ -1,8 +1,29 @@
 import mysql.connector
+from flask import Flask
+
+credentials = {'host': 'localhost',
+               'user': 'root',
+               'password': '',
+               'database': 'devflix'}
 
 
 def create_connection(host, user, password, database):
     return mysql.connector.connect(host=host, user=user, password=password, database=database)
 
 
-create_connection()
+def close_connection(con):
+    return con.close()
+
+
+app = Flask(__name__)  # ----> Forma padrão de inicializar o Flask.
+
+
+app.run()  # ----> Comando para rodar a API
+# conexao = create_connection(credentials['host'], credentials['user'], credentials['password'], credentials['database'])
+# cursor = conexao.cursor()
+# cursor.execute('SELECT * FROM filmes')
+# dados = cursor.fetchall()
+# result = str(dados).split()
+# for item in result:
+#     print(item, end='')
+# close_connection(conexao)
