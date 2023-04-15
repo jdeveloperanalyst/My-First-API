207cb9e640bbd8b9c827952d53b3925eaf49bcb9
jdeveloperanalyst <jonatas1200@icloud.com>
2023-04-14 21:19:34 -0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -5,6 +5,7 @@ credentials = {'host': 'localhost',
                'password': '',
                'database': 'devflix'}
 
+
 def create_connection(host, user, password, database):
     return mysql.connector.connect(host=host, user=user, password=password, database=database)
 
